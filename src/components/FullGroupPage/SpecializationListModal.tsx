b3dffb85b00ffa44303deef89c901797d39abd94
Roman Ptashnyk <2508romaa@example.com>
2024-03-18 23:52:16 +0200

    specialization crud

--- src/components/FullGroupPage/SpecializationListModal.tsx
+++ src/components/FullGroupPage/SpecializationListModal.tsx
@@ -2,44 +2,58 @@ import {
   Stack,
   Table,
   Dialog,
-  Button,
+  Tooltip,
   TableRow,
   TableHead,
   TableCell,
-  Typography,
   TableBody,
+  Typography,
   IconButton,
   DialogTitle,
   DialogContent,
   OutlinedInput,
   TableContainer,
-  Tooltip,
-} from '@mui/material'
-import { CheckOutlined, CloseOutlined, DeleteOutlined } from '@ant-design/icons'
-import React, { Dispatch, SetStateAction } from 'react'
-import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+} from "@mui/material"
+import React, { Dispatch, SetStateAction } from "react"
+import { Controller, SubmitHandler, useForm } from "react-hook-form"
+import {
+  EditOutlined,
+  CheckOutlined,
+  CloseOutlined,
+  DeleteOutlined,
+  CloseSquareOutlined,
+} from "@ant-design/icons"
 
-import { useAppDispatch } from '../../store/store'
-import { createSpecialization } from '../../store/groups/groupsAsyncActions'
-import { useParams } from 'react-router-dom'
+import { useAppDispatch } from "../../store/store"
+import {
+  createSpecialization,
+  deleteSpecialization,
+  updateSpecialization,
+} from "../../store/groups/groupsAsyncActions"
 
 interface ISpecializationListProps {
   open: boolean
+  groupId: number
   specializationList: string[]
   setOpen: Dispatch<SetStateAction<boolean>>
 }
 
 const cellStyles = {
-  borderBottom: '1px solid rgb(220, 220, 220)',
-  p: '4px 10px',
-  minWidth: '100px',
+  borderBottom: "1px solid rgb(220, 220, 220)",
+  p: "4px 10px",
+  minWidth: "100px",
 }
 
-const SpecializationListModal: React.FC<ISpecializationListProps> = ({ open, setOpen, specializationList }) => {
-  const params = useParams()
+const SpecializationListModal: React.FC<ISpecializationListProps> = ({
+  open,
+  groupId,
+  setOpen,
+  specializationList,
+}) => {
   const dispatch = useAppDispatch()
 
-  const [selectedSpecialization, setSelectedSpecialization] = React.useState('')
+  const [selectedSpecialization, setSelectedSpecialization] = React.useState("")
+  const [oldName, setOldName] = React.useState("")
 
   const {
     reset,
@@ -48,7 +62,7 @@ const SpecializationListModal: React.FC<ISpecializationListProps> = ({ open, set
     setValue,
     formState: { errors, isSubmitting },
     handleSubmit,
-  } = useForm<{ name: string }>({ mode: 'onBlur' })
+  } = useForm<{ name: string }>({ mode: "onBlur" })
 
   const handleClose = () => {
     setOpen(false)
@@ -56,55 +70,84 @@ const SpecializationListModal: React.FC<ISpecializationListProps> = ({ open, set
 
   const onSubmit: SubmitHandler<{ name: string }> = async (data) => {
     try {
-      if (!params.id) return
-      dispatch(createSpecialization({ name: data.name, groupId: Number(params.id) }))
-      reset({ name: '' })
+      if (selectedSpecialization) {
+        dispatch(updateSpecialization({ newName: data.name, oldName, groupId }))
+        reset({ name: "" })
+        setSelectedSpecialization("")
+      } else {
+        dispatch(createSpecialization({ name: data.name, groupId }))
+        reset({ name: "" })
+        setSelectedSpecialization("")
+      }
     } catch (error) {
       console.log(error)
+    }
+  }
+
+  const onDeleteSpecGroup = (name: string) => {
+    if (window.confirm("Ви дійсно хочете видалити спец. підгрупу")) {
+      dispatch(deleteSpecialization({ groupId, name }))
     }
   }
 
   return (
     <Dialog
       open={open}
-      maxWidth={'sm'}
+      maxWidth={"sm"}
       onClose={handleClose}
       aria-labelledby="alert-dialog-title"
       aria-describedby="alert-dialog-description"
     >
-      <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-        <DialogTitle>{'Спеціалізовані підгрупи'}</DialogTitle>
+      <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+        <DialogTitle>{"Спеціалізовані підгрупи"}</DialogTitle>
 
         <IconButton sx={{ mt: 1, mr: 1 }} onClick={handleClose}>
           <CloseOutlined />
         </IconButton>
       </div>
-      <DialogContent sx={{ padding: '0 24px 20px' }}>
+      <DialogContent sx={{ padding: "0 24px 20px" }}>
         <TableContainer
           sx={{
-            width: '100%',
-            overflowX: 'auto',
-            position: 'relative',
-            display: 'block',
-            maxWidth: '100%',
-            '& td, & th': { whiteSpace: 'nowrap' },
+            width: "100%",
+            overflowX: "auto",
+            position: "relative",
+            display: "block",
+            maxWidth: "100%",
+            "& td, & th": { whiteSpace: "nowrap" },
           }}
         >
           <Table
             stickyHeader
             aria-labelledby="tableTitle"
             sx={{
-              '& .MuiTableCell-root:first-of-type': { pl: 2 },
-              '& .MuiTableCell-root:last-of-type': { pr: 3 },
+              "& .MuiTableCell-root:first-of-type": { pl: 2 },
+              "& .MuiTableCell-root:last-of-type": { pr: 3 },
             }}
           >
             <TableHead>
               <TableRow>
-                <TableCell sx={{ ...cellStyles, backgroundColor: '#fff !important' }} align="center" padding="none">
+                <TableCell
+                  sx={{ ...cellStyles, backgroundColor: "#fff !important" }}
+                  align="center"
+                  padding="none"
+                >
                   №
                 </TableCell>
-                <TableCell sx={{ ...cellStyles, backgroundColor: '#fff !important' }} align="center" padding="none">
+
+                <TableCell
+                  sx={{ ...cellStyles, backgroundColor: "#fff !important" }}
+                  align="center"
+                  padding="none"
+                >
                   Назва спеціалізованої підгрупи
+                </TableCell>
+
+                <TableCell
+                  sx={{ ...cellStyles, backgroundColor: "#fff !important" }}
+                  align="center"
+                  padding="none"
+                >
+                  Дії
                 </TableCell>
               </TableRow>
             </TableHead>
@@ -120,16 +163,46 @@ const SpecializationListModal: React.FC<ISpecializationListProps> = ({ open, set
                     tabIndex={-1}
                     role="checkbox"
                     selected={row === selectedSpecialization}
-                    onClick={() => {
-                      setValue('name', row)
-                      setSelectedSpecialization(row)
-                    }}
+                    sx={{ ":hover .spec-table-actions": { visibility: "visible !important" } }}
                   >
-                    <TableCell sx={cellStyles} component="th" id={labelId} scope="row" align="center">
+                    <TableCell
+                      sx={cellStyles}
+                      align="center"
+                      component="th"
+                      id={labelId}
+                      scope="row"
+                    >
                       <Typography sx={{ flexGrow: 1 }}>{index + 1}</Typography>
                     </TableCell>
+
                     <TableCell sx={cellStyles} align="center">
                       {row}
+                    </TableCell>
+
+                    <TableCell sx={cellStyles}>
+                      <div className="spec-table-actions" style={{ visibility: "hidden" }}>
+                        <Tooltip title="Редагувати спец. підгрупу">
+                          <IconButton
+                            disabled={isSubmitting}
+                            onClick={() => {
+                              setOldName(row)
+                              setValue("name", row)
+                              setSelectedSpecialization(row)
+                            }}
+                          >
+                            <EditOutlined />
+                          </IconButton>
+                        </Tooltip>
+
+                        <Tooltip title="Видалити спец. підгрупу">
+                          <IconButton
+                            disabled={isSubmitting}
+                            onClick={() => onDeleteSpecGroup(row)}
+                          >
+                            <DeleteOutlined />
+                          </IconButton>
+                        </Tooltip>
+                      </div>
                     </TableCell>
                   </TableRow>
                 )
@@ -140,17 +213,31 @@ const SpecializationListModal: React.FC<ISpecializationListProps> = ({ open, set
 
         <form
           onSubmit={handleSubmit(onSubmit)}
-          style={{ display: 'flex', alignItems: 'cener', justifyContent: 'center', gap: 10, marginTop: '20px' }}
+          style={{
+            display: "flex",
+            alignItems: "cener",
+            justifyContent: "center",
+            gap: 10,
+            marginTop: "20px",
+          }}
         >
-          <Tooltip title="Видалити спец. підгрупу">
-            <IconButton disabled={isSubmitting || !selectedSpecialization}>
-              <DeleteOutlined />
+          <Tooltip
+            title={selectedSpecialization ? "Оновити спец. підгрупу" : "Створити спец. підгрупу"}
+          >
+            <IconButton disabled={isSubmitting} type="submit">
+              <CheckOutlined />
             </IconButton>
           </Tooltip>
 
-          <Tooltip title="Створити спец. підгрупу">
-            <IconButton disabled={!watch('name') || isSubmitting}>
-              <CheckOutlined />
+          <Tooltip title="Відмінити зміни">
+            <IconButton
+              disabled={isSubmitting}
+              onClick={() => {
+                setValue("name", "")
+                setSelectedSpecialization("")
+              }}
+            >
+              <CloseSquareOutlined />
             </IconButton>
           </Tooltip>
 
@@ -165,8 +252,8 @@ const SpecializationListModal: React.FC<ISpecializationListProps> = ({ open, set
                     id="name"
                     {...field}
                     name="name"
-                    placeholder="Назва підгрупи*"
                     error={Boolean(errors.name)}
+                    placeholder="Назва підгрупи*"
                   />
                 </Stack>
               )
--- src/components/FullGroupPage/SpecializationModal.tsx
+++ src/components/FullGroupPage/SpecializationModal.tsx
@@ -17,15 +17,15 @@ import {
   DialogActions,
   TableContainer,
   DialogContentText,
-} from '@mui/material'
-import { useSelector } from 'react-redux'
-import { CheckOutlined, CloseOutlined } from '@ant-design/icons'
-import React, { Dispatch, SetStateAction } from 'react'
+} from "@mui/material"
+import { useSelector } from "react-redux"
+import { CheckOutlined, CloseOutlined } from "@ant-design/icons"
+import React, { Dispatch, SetStateAction } from "react"
 
-import SpecializationListModal from './SpecializationListModal'
-import { groupsSelector } from '../../store/groups/groupsSlice'
-import { groupLessonsByNameAndSemester } from '../../utils/groupLessonsByNameAndSemester'
-import { GroupLoadType } from '../../store/groups/groupsTypes'
+import SpecializationListModal from "./SpecializationListModal"
+import { groupsSelector } from "../../store/groups/groupsSlice"
+import { groupLessonsByNameAndSemester } from "../../utils/groupLessonsByNameAndSemester"
+import { GroupLoadType } from "../../store/groups/groupsTypes"
 
 interface ISelectPlanModalProps {
   open: boolean
@@ -33,9 +33,9 @@ interface ISelectPlanModalProps {
 }
 
 const cellStyles = {
-  borderBottom: '1px solid rgb(220, 220, 220)',
-  p: '4px 10px',
-  minWidth: '100px',
+  borderBottom: "1px solid rgb(220, 220, 220)",
+  p: "4px 10px",
+  minWidth: "100px",
 }
 
 const SpecializationModal: React.FC<ISelectPlanModalProps> = ({ open, setOpen }) => {
@@ -53,6 +53,7 @@ const SpecializationModal: React.FC<ISelectPlanModalProps> = ({ open, setOpen })
   return (
     <>
       <SpecializationListModal
+        groupId={group.id}
         open={specializationListModalVisible}
         setOpen={setSpecializationListModalVisible}
         specializationList={group.specializationList}
@@ -61,28 +62,32 @@ const SpecializationModal: React.FC<ISelectPlanModalProps> = ({ open, setOpen })
       <Dialog
         open={open}
         // fullScreen
-        maxWidth={'xl'}
+        maxWidth={"xl"}
         onClose={handleClose}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-          <DialogTitle>{'Спеціалізовані підгрупи'}</DialogTitle>
+        <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
+          <DialogTitle>{"Спеціалізовані підгрупи"}</DialogTitle>
 
-          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+          <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
             <Stack>
               <TextField
                 select
                 fullWidth
                 id="category"
-                defaultValue={'empty'}
+                defaultValue={"empty"}
                 //   value={value}
                 //   onChange={(e) => setValue(e.target.value)}
-                sx={{ width: '240px', p: '16px 8px', '& .MuiInputBase-input': { py: '10.4px', fontSize: '0.875rem' } }}
+                sx={{
+                  width: "240px",
+                  p: "16px 8px",
+                  "& .MuiInputBase-input": { py: "10.4px", fontSize: "0.875rem" },
+                }}
               >
                 {[
-                  { label: 'Спеціалізація відсутня', value: 'empty' },
-                  { label: 'Не вивчається', value: 'is-not-studied' },
+                  { label: "Спеціалізація відсутня", value: "empty" },
+                  { label: "Не вивчається", value: "is-not-studied" },
                   ...group.specializationList.map((el) => ({ label: el, value: el })),
                 ].map((category) => (
                   <MenuItem key={category.value} value={category.value}>
@@ -93,39 +98,39 @@ const SpecializationModal: React.FC<ISelectPlanModalProps> = ({ open, setOpen })
             </Stack>
 
             <Tooltip title="Підтвердити зміни">
-              <IconButton sx={{ mr: '24px' }}>
+              <IconButton sx={{ mr: "24px" }}>
                 <CheckOutlined />
               </IconButton>
             </Tooltip>
           </div>
 
-          <IconButton sx={{ mr: '24px' }} onClick={handleClose}>
+          <IconButton sx={{ mr: "24px" }} onClick={handleClose}>
             <CloseOutlined />
           </IconButton>
         </div>
-        <DialogContent sx={{ padding: '0 24px 20px' }}>
+        <DialogContent sx={{ padding: "0 24px 20px" }}>
           <TableContainer
             sx={{
-              width: '100%',
-              display: 'block',
-              maxWidth: '100%',
-              overflowX: 'auto',
-              position: 'relative',
-              '& td, & th': { whiteSpace: 'nowrap' },
+              width: "100%",
+              display: "block",
+              maxWidth: "100%",
+              overflowX: "auto",
+              position: "relative",
+              "& td, & th": { whiteSpace: "nowrap" },
             }}
           >
             <Table
               stickyHeader
               aria-labelledby="tableTitle"
               sx={{
-                '& .MuiTableCell-root:first-of-type': { pl: 2 },
-                '& .MuiTableCell-root:last-of-type': { pr: 3 },
+                "& .MuiTableCell-root:first-of-type": { pl: 2 },
+                "& .MuiTableCell-root:last-of-type": { pr: 3 },
               }}
             >
               <TableHead>
                 <TableRow>
                   <TableCell
-                    sx={{ ...cellStyles, backgroundColor: '#fff !important' }}
+                    sx={{ ...cellStyles, backgroundColor: "#fff !important" }}
                     align="center"
                     rowSpan={3}
                     padding="none"
@@ -133,7 +138,7 @@ const SpecializationModal: React.FC<ISelectPlanModalProps> = ({ open, setOpen })
                     Дисципліна
                   </TableCell>
                   <TableCell
-                    sx={{ ...cellStyles, backgroundColor: '#fff !important' }}
+                    sx={{ ...cellStyles, backgroundColor: "#fff !important" }}
                     align="center"
                     rowSpan={3}
                     padding="none"
@@ -141,7 +146,7 @@ const SpecializationModal: React.FC<ISelectPlanModalProps> = ({ open, setOpen })
                     Семестр
                   </TableCell>
                   <TableCell
-                    sx={{ ...cellStyles, backgroundColor: '#fff !important' }}
+                    sx={{ ...cellStyles, backgroundColor: "#fff !important" }}
                     align="center"
                     rowSpan={3}
                     padding="none"
@@ -150,35 +155,35 @@ const SpecializationModal: React.FC<ISelectPlanModalProps> = ({ open, setOpen })
                   </TableCell>
 
                   <TableCell
-                    sx={{ ...cellStyles, p: 1, backgroundColor: '#fff !important' }}
+                    sx={{ ...cellStyles, p: 1, backgroundColor: "#fff !important" }}
                     align="center"
                     padding="none"
                   >
                     Лекції
                   </TableCell>
                   <TableCell
-                    sx={{ ...cellStyles, p: 1, backgroundColor: '#fff !important' }}
+                    sx={{ ...cellStyles, p: 1, backgroundColor: "#fff !important" }}
                     align="center"
                     padding="none"
                   >
                     Практичні
                   </TableCell>
                   <TableCell
-                    sx={{ ...cellStyles, p: 1, backgroundColor: '#fff !important' }}
+                    sx={{ ...cellStyles, p: 1, backgroundColor: "#fff !important" }}
                     align="center"
                     padding="none"
                   >
                     Лабораторні
                   </TableCell>
                   <TableCell
-                    sx={{ ...cellStyles, p: 1, backgroundColor: '#fff !important' }}
+                    sx={{ ...cellStyles, p: 1, backgroundColor: "#fff !important" }}
                     align="center"
                     padding="none"
                   >
                     Семінари
                   </TableCell>
                   <TableCell
-                    sx={{ ...cellStyles, p: 1, backgroundColor: '#fff !important' }}
+                    sx={{ ...cellStyles, p: 1, backgroundColor: "#fff !important" }}
                     align="center"
                     padding="none"
                   >
@@ -189,82 +194,91 @@ const SpecializationModal: React.FC<ISelectPlanModalProps> = ({ open, setOpen })
 
               <TableBody>
                 {/* {groupLessonsByName(plan.subjects).map((row, index: number) => { */}
-                {(!group.groupLoad ? [] : groupLessonsByNameAndSemester(group.groupLoad)).map((row, index: number) => {
-                  const isItemSelected = selectedLesson?.id === row[0].id
-                  const labelId = `enhanced-table-checkbox-${index}`
+                {(!group.groupLoad ? [] : groupLessonsByNameAndSemester(group.groupLoad)).map(
+                  (row, index: number) => {
+                    const isItemSelected = selectedLesson?.id === row[0].id
+                    const labelId = `enhanced-table-checkbox-${index}`
 
-                  return (
-                    <TableRow
-                      hover
-                      tabIndex={-1}
-                      role="checkbox"
-                      selected={isItemSelected}
-                      aria-checked={isItemSelected}
-                      key={row[0].name + row[0].semester}
-                      onClick={() => setSelectedLesson(row[0])}
-                    >
-                      <TableCell
-                        sx={{
-                          ...cellStyles,
-                          minWidth: '250px !important',
-                          position: 'relative',
-                          ':hover *': { display: 'flex !important' },
-                        }}
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        align="left"
+                    return (
+                      <TableRow
+                        hover
+                        tabIndex={-1}
+                        role="checkbox"
+                        selected={isItemSelected}
+                        aria-checked={isItemSelected}
+                        key={row[0].name + row[0].semester}
+                        onClick={() => setSelectedLesson(row[0])}
                       >
-                        <Typography sx={{ flexGrow: 1 }}>{row[0].name}</Typography>
-                      </TableCell>
-                      <TableCell sx={cellStyles} align="center">
-                        {row[0].semester}
-                      </TableCell>
+                        <TableCell
+                          sx={{
+                            ...cellStyles,
+                            minWidth: "250px !important",
+                            position: "relative",
+                            ":hover *": { display: "flex !important" },
+                          }}
+                          component="th"
+                          id={labelId}
+                          scope="row"
+                          align="left"
+                        >
+                          <Typography sx={{ flexGrow: 1 }}>{row[0].name}</Typography>
+                        </TableCell>
+                        <TableCell sx={cellStyles} align="center">
+                          {row[0].semester}
+                        </TableCell>
 
-                      <TableCell
-                        key={index}
-                        sx={{
-                          ...cellStyles,
-                          cursor: 'pointer',
-                          ':hover': { background: 'rgba(0, 0, 0, 0.05);' },
-                        }}
-                        align="center"
-                      >
-                        {row[0].specialization ? row[0].specialization : '-'}
-                      </TableCell>
+                        <TableCell
+                          key={index}
+                          sx={{
+                            ...cellStyles,
+                            cursor: "pointer",
+                            ":hover": { background: "rgba(0, 0, 0, 0.05);" },
+                          }}
+                          align="center"
+                        >
+                          {row[0].specialization ? row[0].specialization : "-"}
+                        </TableCell>
 
-                      {['lectures', 'practical', 'laboratory', 'seminars', 'exams'].map((lessonType, index) => {
-                        const lesson = row.find((el) => el.typeEn === lessonType)
+                        {["lectures", "practical", "laboratory", "seminars", "exams"].map(
+                          (lessonType, index) => {
+                            const lesson = row.find((el) => el.typeEn === lessonType)
 
-                        return (
-                          <TableCell
-                            key={index}
-                            onClick={() => {}}
-                            sx={{
-                              ...cellStyles,
-                              cursor: 'pointer',
-                            }}
-                            align="center"
-                          >
-                            {lesson ? lesson.hours : ''}
-                          </TableCell>
-                        )
-                      })}
-                    </TableRow>
-                  )
-                })}
+                            return (
+                              <TableCell
+                                key={index}
+                                onClick={() => {}}
+                                sx={{
+                                  ...cellStyles,
+                                  cursor: "pointer",
+                                }}
+                                align="center"
+                              >
+                                {lesson ? lesson.hours : ""}
+                              </TableCell>
+                            )
+                          }
+                        )}
+                      </TableRow>
+                    )
+                  }
+                )}
               </TableBody>
             </Table>
           </TableContainer>
         </DialogContent>
 
-        <DialogActions sx={{ paddingBottom: '24px' }}>
+        <DialogActions sx={{ paddingBottom: "24px" }}>
           <Button
             type="submit"
             color="primary"
             variant="outlined"
             onClick={() => setSpecializationListModalVisible(true)}
-            sx={{ textTransform: 'capitalize', marginRight: 'auto', marginLeft: '16px', p: '7.44px 15px' }}
+            sx={{
+              textTransform: "capitalize",
+              marginRight: "auto",
+              marginLeft: "16px",
+              p: "7.44px 15px",
+            }}
           >
             Редагувати спеціалізовані підгрупи
           </Button>
@@ -275,11 +289,11 @@ const SpecializationModal: React.FC<ISelectPlanModalProps> = ({ open, setOpen })
             variant="outlined"
             onClick={handleClose}
             sx={{
-              textTransform: 'capitalize',
-              maxWidth: '140px',
-              marginLeft: 'auto',
-              p: '7.44px 15px',
-              width: '100%',
+              textTransform: "capitalize",
+              maxWidth: "140px",
+              marginLeft: "auto",
+              p: "7.44px 15px",
+              width: "100%",
             }}
           >
             Закрити
