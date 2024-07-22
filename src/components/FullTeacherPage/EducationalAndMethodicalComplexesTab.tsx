0dfa72361b8c9bed8678cb107a54af3d3747e538
Roman Ptashnyk <2508romaa@example.com>
2024-07-22 23:54:41 +0300

    teacher profile

--- src/components/FullTeacherPage/EducationalAndMethodicalComplexesTab.tsx
+++ src/components/FullTeacherPage/EducationalAndMethodicalComplexesTab.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { CheckOutlined, CloseOutlined, EditOutlined } from '@ant-design/icons'
 import {
-  Button,
-  Divider,
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
   Table,
+  Button,
+  Select,
+  Divider,
+  Tooltip,
+  MenuItem,
+  TableRow,
   TableBody,
   TableCell,
-  TableContainer,
   TableHead,
-  TableRow,
   TextField,
-  Tooltip,
-} from '@mui/material'
+  InputLabel,
+  FormControl,
+  IconButton,
+} from "@mui/material"
+import React from "react"
+import { CheckOutlined, CloseOutlined, EditOutlined } from "@ant-design/icons"
 
 type Props = {}
 
@@ -24,30 +24,32 @@ function createData(id: number, name: string, hours: number) {
 }
 
 const rows = [
-  createData(1, 'Інформація, інформаційні технології та людина в інформаційному суспільстві', 2),
-  createData(2, 'Інформаційна безпека та основи кібергігієни', 2),
-  createData(3, 'Цифрове навчання та комп’ютерно-орієнтовані засоби навчальної діяльності', 2),
-  createData(4, 'Сучасні інформаційні технології та їх вплив на суспільство', 2),
+  createData(1, "Інформація, інформаційні технології та людина в інформаційному суспільстві", 2),
+  createData(2, "Інформаційна безпека та основи кібергігієни", 2),
+  createData(3, "Цифрове навчання та комп’ютерно-орієнтовані засоби навчальної діяльності", 2),
+  createData(4, "Сучасні інформаційні технології та їх вплив на суспільство", 2),
   createData(5, "Комп’ютерне моделювання та комп'ютерний експеримент", 2),
 ]
 
+const editedThemeInitialState = { id: -1, name: "" }
+
 export const EducationalAndMethodicalComplexesTab = ({}: Props) => {
-  const [editedTheme, setEditedTheme] = React.useState<{ id: number; name: string }>({ id: -1, name: '' })
+  const [editedTheme, setEditedTheme] = React.useState<{ id: number; name: string }>(editedThemeInitialState)
 
   return (
     <div>
-      <div style={{ display: 'flex', alignItems: 'center' }}>
+      <div style={{ display: "flex", alignItems: "center" }}>
         <FormControl fullWidth sx={{ my: 3 }}>
-          <InputLabel sx={{ overflow: 'visible !important' }}>Група</InputLabel>
+          <InputLabel sx={{ overflow: "visible !important" }}>Група</InputLabel>
           <Select
           // onChange={handleChangeEditedUser}
           // value={editedUser ? String(editedUser.id) : ''}
           >
             {[
-              { id: 1, name: 'LD9-22-1' },
-              { id: 2, name: 'PH9-22-1' },
-              { id: 3, name: 'PH9-23-1' },
-              { id: 4, name: 'PH9-24-1' },
+              { id: 1, name: "LD9-22-1" },
+              { id: 2, name: "PH9-22-1" },
+              { id: 3, name: "PH9-23-1" },
+              { id: 4, name: "PH9-24-1" },
             ].map((el) => (
               <MenuItem value={el.id}>{el.name}</MenuItem>
             ))}
@@ -55,23 +57,23 @@ export const EducationalAndMethodicalComplexesTab = ({}: Props) => {
         </FormControl>
 
         <FormControl fullWidth sx={{ my: 3, ml: 2 }}>
-          <InputLabel sx={{ overflow: 'visible !important' }}>Дисципліна</InputLabel>
+          <InputLabel sx={{ overflow: "visible !important" }}>Дисципліна</InputLabel>
           <Select
           // onChange={handleChangeEditedUser}
           // value={editedUser ? String(editedUser.id) : ''}
           >
             {[
-              { id: 1, name: 'Інформатика' },
-              { id: 2, name: 'Інформаційні технології у фармації' },
-              { id: 3, name: 'Технології' },
-              { id: 4, name: 'Основи медичної інформатики' },
+              { id: 1, name: "Інформатика" },
+              { id: 2, name: "Інформаційні технології у фармації" },
+              { id: 3, name: "Технології" },
+              { id: 4, name: "Основи медичної інформатики" },
             ].map((el) => (
               <MenuItem value={el.id}>{el.name}</MenuItem>
             ))}
           </Select>
         </FormControl>
 
-        <Button variant="contained" sx={{ height: '41px', minWidth: '120px', ml: 2 }}>
+        <Button variant="contained" sx={{ height: "41px", minWidth: "120px", ml: 2 }}>
           Переглянути
         </Button>
       </div>
@@ -89,30 +91,28 @@ export const EducationalAndMethodicalComplexesTab = ({}: Props) => {
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.id}>
-              <TableCell component="th" sx={{ width: '80px', height: '48px', padding: '0px 12px' }}>
+              <TableCell component="th" sx={{ width: "80px", height: "48px", padding: "0px 12px" }}>
                 {row.id}
               </TableCell>
 
               <TableCell
                 align="left"
                 sx={{
-                  '&:hover span': { display: 'inline-block !important' },
-                  display: 'flex',
-                  alignItems: 'center',
-                  height: '48px',
-                  padding: '0px 12px',
+                  "&:hover span": { display: "inline-block !important" },
+                  display: "flex",
+                  alignItems: "center",
+                  height: "48px",
+                  padding: "0px 12px",
                 }}
               >
-                {/* {row.name} */}
-
                 {editedTheme.id === row.id ? (
                   <div style={{ flexGrow: 1 }}>
                     <TextField
                       fullWidth
                       sx={{
-                        '.MuiInputBase-input': { padding: '0 14px 0 4px', height: '31px', width: '100%' },
-                        '& .Mui-focused': { boxShadow: 'none !important' },
-                        fieldset: { border: '0 !important' },
+                        ".MuiInputBase-input": { padding: "0 14px 0 4px", height: "31px", width: "100%" },
+                        "& .Mui-focused": { boxShadow: "none !important" },
+                        fieldset: { border: "0 !important" },
                       }}
                       value={editedTheme.name}
                     />
@@ -121,27 +121,26 @@ export const EducationalAndMethodicalComplexesTab = ({}: Props) => {
                   <p style={{ flexGrow: 1, margin: 0 }}>{row.name}</p>
                 )}
 
-                <div style={{ width: '28px', display: 'flex', justifyContent: 'center' }}>
+                <div style={{ width: "28px", display: "flex", justifyContent: "center" }}>
                   {editedTheme.id === row.id ? (
                     <>
-                      <Tooltip title="Зберегти">
-                        <CheckOutlined style={{ cursor: 'pointer', marginRight: '10px' }} />
-                      </Tooltip>
+                      <IconButton sx={{ marginRight: "10px" }} onClick={() => setEditedTheme(editedThemeInitialState)}>
+                        <CheckOutlined style={{ cursor: "pointer" }} />
+                      </IconButton>
 
-                      <Tooltip title="Відмінити">
-                        <CloseOutlined style={{ cursor: 'pointer' }} />
-                      </Tooltip>
+                      <IconButton onClick={() => setEditedTheme(editedThemeInitialState)}>
+                        <CloseOutlined style={{ cursor: "pointer" }} />
+                      </IconButton>
                     </>
                   ) : (
-                    <EditOutlined
-                      style={{ display: 'none', cursor: 'pointer' }}
-                      onClick={() => setEditedTheme({ id: row.id, name: row.name })}
-                    />
+                    <IconButton onClick={() => setEditedTheme({ id: row.id, name: row.name })}>
+                      <EditOutlined style={{ display: "none", cursor: "pointer" }} />
+                    </IconButton>
                   )}
                 </div>
               </TableCell>
 
-              <TableCell sx={{ width: '100px', height: '48px', padding: '0px 12px' }} align="center">
+              <TableCell sx={{ width: "100px", height: "48px", padding: "0px 12px" }} align="center">
                 {row.hours}
               </TableCell>
             </TableRow>
@@ -149,12 +148,12 @@ export const EducationalAndMethodicalComplexesTab = ({}: Props) => {
         </TableBody>
       </Table>
 
-      <table className="full-teacher__teaching-load-table" cellSpacing="0">
+      {/* <table className="full-teacher__teaching-load-table" cellSpacing="0">
         <thead>
           <tr>
-            <th style={{ width: '60px' }}>№</th>
+            <th style={{ width: "60px" }}>№</th>
             <th>Тема</th>
-            <th style={{ width: '140px' }}>Години</th>
+            <th style={{ width: "140px" }}>Години</th>
           </tr>
         </thead>
         <tbody>
@@ -167,13 +166,13 @@ export const EducationalAndMethodicalComplexesTab = ({}: Props) => {
                   style={
                     editedTheme.id === rowIndex
                       ? {
-                          textAlign: 'left',
-                          display: 'flex',
-                          alignItems: 'center',
-                          padding: '0 15px 0 0',
-                          height: '31px',
+                          textAlign: "left",
+                          display: "flex",
+                          alignItems: "center",
+                          padding: "0 15px 0 0",
+                          height: "31px",
                         }
-                      : { textAlign: 'left', display: 'flex', alignItems: 'center', paddingRight: '15px' }
+                      : { textAlign: "left", display: "flex", alignItems: "center", paddingRight: "15px" }
                   }
                 >
                   {editedTheme.id === rowIndex ? (
@@ -181,9 +180,9 @@ export const EducationalAndMethodicalComplexesTab = ({}: Props) => {
                       <TextField
                         fullWidth
                         sx={{
-                          '.MuiInputBase-input': { padding: '0 14px 0 4px', height: '31px', width: '100%' },
-                          '& .Mui-focused': { boxShadow: 'none !important' },
-                          fieldset: { border: '0 !important' },
+                          ".MuiInputBase-input": { padding: "0 14px 0 4px", height: "31px", width: "100%" },
+                          "& .Mui-focused": { boxShadow: "none !important" },
+                          fieldset: { border: "0 !important" },
                         }}
                         value={editedTheme.name}
                       />
@@ -192,21 +191,21 @@ export const EducationalAndMethodicalComplexesTab = ({}: Props) => {
                     <p style={{ flexGrow: 1, margin: 0 }}>Інформаційні технології в суспільстві</p>
                   )}
 
-                  <div style={{ width: '28px', display: 'flex', justifyContent: 'center' }}>
+                  <div style={{ width: "28px", display: "flex", justifyContent: "center" }}>
                     {editedTheme.id === rowIndex ? (
                       <>
                         <Tooltip title="Зберегти">
-                          <CheckOutlined style={{ cursor: 'pointer', marginRight: '10px' }} />
+                          <CheckOutlined style={{ cursor: "pointer", marginRight: "10px" }} />
                         </Tooltip>
 
                         <Tooltip title="Відмінити">
-                          <CloseOutlined style={{ cursor: 'pointer' }} />
+                          <CloseOutlined style={{ cursor: "pointer" }} />
                         </Tooltip>
                       </>
                     ) : (
                       <EditOutlined
-                        style={{ display: 'none', cursor: 'pointer' }}
-                        onClick={() => setEditedTheme({ id: rowIndex, name: 'Інформаційні технології в суспільстві' })}
+                        style={{ display: "none", cursor: "pointer" }}
+                        onClick={() => setEditedTheme({ id: rowIndex, name: "Інформаційні технології в суспільстві" })}
                       />
                     )}
                   </div>
@@ -216,13 +215,13 @@ export const EducationalAndMethodicalComplexesTab = ({}: Props) => {
             ))}
 
           <tr>
-            <th colSpan={2} style={{ textAlign: 'left' }}>
+            <th colSpan={2} style={{ textAlign: "left" }}>
               Всього
             </th>
             <th>24</th>
           </tr>
         </tbody>
-      </table>
+      </table> */}
     </div>
   )
 }
--- src/components/FullTeacherPage/MyTeachingLoadTab.tsx
+++ src/components/FullTeacherPage/MyTeachingLoadTab.tsx
@@ -1,83 +1,214 @@
-import React from 'react'
+import React from "react"
+import { Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material"
 
-import './FullTeacherPage.css'
+import "./FullTeacherPage.css"
 
 interface Props {}
 
 const lessonsCount = 6
 
-const lessonTypes = ['ЛК', 'ПЗ', 'ЛАБ', 'СЕМ', 'ЕКЗ', 'КОНС', 'МЕТОД']
+// Група	Дисципліна	Група/Підгрупа	Вид заняття	Години
+function createData(
+  id: number,
+  group: string,
+  lessonName: string,
+  category: string,
+  lessonType: string,
+  hours: number
+) {
+  return { id, group, lessonName, category, lessonType, hours }
+}
+
+const rows = [
+  createData(1, "PH9-23-1", "Інформаційні технології у фармації", "Вся група", "ЛК", 24),
+  createData(2, "PH9-23-1", "Інформаційні технології у фармації", "Вся група", "ЛК", 24),
+  createData(3, "PH9-23-1", "Інформаційні технології у фармації", "Вся група", "ЛК", 24),
+  createData(4, "PH9-23-1", "Інформаційні технології у фармації", "Вся група", "ЛК", 24),
+  createData(5, "PH9-23-1", "Інформаційні технології у фармації", "Вся група", "ЛК", 24),
+  createData(6, "PH9-23-1", "Інформаційні технології у фармації", "Вся група", "ЛК", 24),
+]
+
+const sellStyles = {
+  whiteSpace: "nowrap",
+  textOverflow: "ellipsis",
+  overflow: "hidden",
+  padding: "6px",
+  height: "40px",
+}
 
 export const MyTeachingLoadTab: React.FC<Props> = ({}) => {
   return (
-    <table className="full-teacher__teaching-load-table" cellSpacing="0">
-      <thead>
-        <tr>
-          <th style={{ width: '100px' }}>Група</th>
-          <th>Дисципліна</th>
-          <th>Група/Підгрупа</th>
-          <th>Вид заняття</th>
-          <th>Години</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th colSpan={lessonsCount + 1}>Семестр 1</th>
-        </tr>
-        {Array(6)
-          .fill(null)
-          .map((_, rowIndex) => (
-            <tr key={rowIndex}>
-              <td>PH9-22-1</td>
-              <td style={{ textAlign: 'left' }}>Органічна хімія</td>
-              <td>Вся група</td>
-              <td>Лекції</td>
-              <td>24</td>
-            </tr>
+    <>
+      <Table sx={{ minWidth: 450, mb: 10 }} size="small" aria-label="a dense table">
+        <TableHead>
+          <TableRow>
+            <TableCell align="center">№</TableCell>
+            <TableCell align="center">Група</TableCell>
+            <TableCell align="center">Дисципліна</TableCell>
+            <TableCell align="center">Група/Підгрупа</TableCell>
+            <TableCell align="center">Вид заняття</TableCell>
+            <TableCell align="center">Години</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {/*  */}
+          <TableRow>
+            <TableCell colSpan={6} align="center" component="th" sx={{ fontWeight: 600 }}>
+              Семестр 1
+            </TableCell>
+          </TableRow>
+
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell align="center" sx={{ ...sellStyles }}>
+                {row.id}
+              </TableCell>
+
+              <TableCell align="center" sx={{ ...sellStyles }}>
+                {row.group}
+              </TableCell>
+
+              <TableCell align="center" sx={{ ...sellStyles }}>
+                {row.lessonName}
+              </TableCell>
+
+              <TableCell align="center" sx={{ ...sellStyles }}>
+                {row.category}
+              </TableCell>
+
+              <TableCell align="center" sx={{ ...sellStyles }}>
+                {row.lessonType}
+              </TableCell>
+
+              <TableCell sx={{ width: "100px", ...sellStyles }} align="center">
+                {row.hours}
+              </TableCell>
+            </TableRow>
           ))}
 
-        <tr>
-          <th colSpan={4} style={{ textAlign: 'left' }}>
-            Всього за 1 семестр
-          </th>
-          <th>240</th>
-        </tr>
+          <TableRow>
+            <TableCell colSpan={5} component="th" align="left" sx={{ fontWeight: 600 }}>
+              Всього за 1 семестр
+            </TableCell>
+            <TableCell align="center" component="th" sx={{ fontWeight: 600 }}>
+              360
+            </TableCell>
+          </TableRow>
 
-        <tr>
-          <th colSpan={5} style={{ padding: '.1px' }}></th>
-        </tr>
+          <TableRow>
+            <TableCell colSpan={6} component="th" align="center" sx={{ fontWeight: 600 }}>
+              Семестр 2
+            </TableCell>
+          </TableRow>
 
-        <tr>
-          <th colSpan={lessonsCount + 1}>Семестр 2</th>
-        </tr>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell align="center" sx={{ ...sellStyles }}>
+                {row.id}
+              </TableCell>
 
-        {Array(6)
-          .fill(null)
-          .map((_, rowIndex) => (
-            <tr key={rowIndex}>
-              <td>PH9-22-1</td>
-              <td style={{ textAlign: 'left' }}>Органічна хімія</td>
-              <td>Вся група</td>
-              <td>Лекції</td>
-              <td>24</td>
-            </tr>
+              <TableCell align="center" sx={{ ...sellStyles }}>
+                {row.group}
+              </TableCell>
+
+              <TableCell align="center" sx={{ ...sellStyles }}>
+                {row.lessonName}
+              </TableCell>
+
+              <TableCell align="center" sx={{ ...sellStyles }}>
+                {row.category}
+              </TableCell>
+
+              <TableCell align="center" sx={{ ...sellStyles }}>
+                {row.lessonType}
+              </TableCell>
+
+              <TableCell sx={{ width: "100px", ...sellStyles }} align="center">
+                {row.hours}
+              </TableCell>
+            </TableRow>
           ))}
 
-        <tr>
-          <th colSpan={4} style={{ textAlign: 'left' }}>
-            Всього за 2 семестр
-          </th>
-          <th>320</th>
-        </tr>
+          <TableRow>
+            <TableCell colSpan={5} component="th" align="left" sx={{ fontWeight: 600 }}>
+              Всього за 2 семестр
+            </TableCell>
+            <TableCell align="center" component="th" sx={{ fontWeight: 600 }}>
+              360
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
 
-        <tr>
-          <th colSpan={4} style={{ textAlign: 'left' }}>
-            Всього за рік
-          </th>
-          <th>560</th>
-        </tr>
-      </tbody>
-    </table>
+      <table className="full-teacher__teaching-load-table" cellSpacing="0">
+        <thead>
+          <tr>
+            <th style={{ width: "100px" }}>Група</th>
+            <th>Дисципліна</th>
+            <th>Група/Підгрупа</th>
+            <th>Вид заняття</th>
+            <th>Години</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th colSpan={lessonsCount + 1}>Семестр 1</th>
+          </tr>
+          {Array(6)
+            .fill(null)
+            .map((_, rowIndex) => (
+              <tr key={rowIndex}>
+                <td>PH9-22-1</td>
+                <td style={{ textAlign: "left" }}>Органічна хімія</td>
+                <td>Вся група</td>
+                <td>Лекції</td>
+                <td>24</td>
+              </tr>
+            ))}
+
+          <tr>
+            <th colSpan={4} style={{ textAlign: "left" }}>
+              Всього за 1 семестр
+            </th>
+            <th>240</th>
+          </tr>
+
+          <tr>
+            <th colSpan={5} style={{ padding: ".1px" }}></th>
+          </tr>
+
+          <tr>
+            <th colSpan={lessonsCount + 1}>Семестр 2</th>
+          </tr>
+
+          {Array(6)
+            .fill(null)
+            .map((_, rowIndex) => (
+              <tr key={rowIndex}>
+                <td>PH9-22-1</td>
+                <td style={{ textAlign: "left" }}>Органічна хімія</td>
+                <td>Вся група</td>
+                <td>Лекції</td>
+                <td>24</td>
+              </tr>
+            ))}
+
+          <tr>
+            <th colSpan={4} style={{ textAlign: "left" }}>
+              Всього за 2 семестр
+            </th>
+            <th>320</th>
+          </tr>
+
+          <tr>
+            <th colSpan={4} style={{ textAlign: "left" }}>
+              Всього за рік
+            </th>
+            <th>560</th>
+          </tr>
+        </tbody>
+      </table>
+    </>
   )
 }
 
